9b41388146a04852bd3444a0c9da5073724040e2
Ben LaRoque <benjamin.laroque@gmail.com>
2018-11-20 21:22:14 -0800

    more class structuring

--- seattle_sunrise/light_control.py
+++ seattle_sunrise/light_control.py
@@ -19,7 +19,7 @@ headers = {
     'Authorization': 'Bearer BABBLE_FISH',
     'content-type': 'application/json',
 }
-requests.post(url_base+turn_on_target, json=json.dumps({'brightness':1, 'entity_id': light_entity}))
+#requests.post(url_base+turn_on_target, json=json.dumps({'brightness':1, 'entity_id': light_entity}))
 
 __all__.append("LightFlicker")
 class LightFlicker:
@@ -49,38 +49,47 @@ __all__.append("event_loop_control")
 class event_loop_control():
     def __init__(self):
         self.loop = asyncio.get_event_loop()
+        self.event_poll_interval = 10 #seconds
+
+        # pre-schedule an event so I can see it get removed
+        self.tasks = {'fail': self.loop.call_at(self.loop.time()+10000, light_flicker.print_time)}
 
     def __call__(self):
         self.loop.run_until_complete(self.main_loop())
         self.loop.close()
+
+    def _update_tasks(self, new_tasks):
+
+        # make sure that every item in new_tasks exists in self.tasks (add if missing)
+        for id,a_time in new_tasks.items():
+            if not id in self.tasks:
+                when_to_call = self.loop.time()+(a_time-datetime.datetime.now()).total_seconds()
+                self.tasks[id] = self.loop.call_at(when_to_call, light_flicker.print_time)
+        print('new tasks added')
+
+        # make sure that all existing tasks are in new_tasks (cancel and remove if missing)
+        to_pop = []
+        for id in self.tasks:
+            if not id in new_tasks:
+                self.tasks[id].cancel()
+                to_pop.append(id)
+        for id in to_pop:
+            self.tasks.pop(id)
 
     async def main_loop(self):
         # make instances
         light_flicker = LightFlicker()
         event_getter = EventGetter()
 
-        # pre-schedule an event so I can see it get removed
-        tasks = {'fail': self.loop.call_at(self.loop.time()+10000, light_flicker.print_time)}
-
         count = 0
         while True:
-            print('tasks are: ', tasks)
+            print('existing tasks are: ', self.tasks)
             new_events = event_getter.get_events()
-            for id,a_time in new_events.items():
-                if not id in tasks:
-                    when_to_call = self.loop.time()+(a_time-datetime.datetime.now()).total_seconds()
-                    tasks[id] = self.loop.call_at(when_to_call, light_flicker.print_time)
-            print('new tasks added')
-            to_pop = []
-            for id in tasks:
-                if not id in new_events:
-                    tasks[id].cancel()
-                    to_pop.append(id)
-            [tasks.pop(id) for id in to_pop]
-            print("removed tasks purged")
+            print("task list updated")
             count += 1
-            print('about to sleep again: ', count)
-            await asyncio.sleep(10)
+            print('about to sleep for the {}th time: '.format(count))
+            await asyncio.sleep(self.event_poll_interval)
+            # for now, break after 10 loops, remove this when first version comes out
             if count > 10:
                 break
 
