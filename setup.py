7fc5496547a76ba4aba6edb9bea06bb104e5aa71
Kimberly Schlesinger <kschlesinger@slartibartfast>
2018-12-09 18:55:41 -0800

    fixed typos

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     install_requires = [
         'requests',
         'PyYAML',
-        'googleapiclient',
+        'google-api-python-client',
         'httplib2',
         'oauth2client',
     ],
