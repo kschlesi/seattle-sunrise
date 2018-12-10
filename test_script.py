d8dd296639900bc6d2285afa0d4e97b36f75330c
Kim Schlesinger <kimberly.schlesinger@realself.com>
2018-12-09 22:20:49 -0800

    correct calendar name

---
+++ test_script.py
@@ -0,0 +1,10 @@
+import seattle_sunrise
+
+cr = seattle_sunrise.CalendarReader(credendials_path='/var/seattle_sunrise/secrets')
+
+all_events = cr.get_scheduled_events()
+
+bl_events = cr.get_scheduled_events(device_name='Bedroom Light')
+
+print(all_events)
+print(bl_events)
